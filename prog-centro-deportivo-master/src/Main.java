cd0e080074d4847893fd4e3fd94933ef14e1562b
Alejandro <amelevel@myuax.com>
2026-03-04 00:29:09 +0100

    chore main

--- prog-centro-deportivo-master/src/Main.java
+++ prog-centro-deportivo-master/src/Main.java
@@ -1,9 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.lang.classfile.instruction.NewMultiArrayInstruction;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        CentroDeportivo centroDeportivo = new CentroDeportivo(15);
+
+        Entrenador entrenador1 = new Entrenador(1, "Julian", "Fisiatra", 2);
+        Entrenador entrenador2 = new Entrenador(2, "Luis", "Piernas", 3);
+        Entrenador entrenador3 = new Entrenador(1, "Carlos", "Brazos", 1);
+
+        System.out.println(centroDeportivo.buscarEntrenador(1) ) ;  // null
+
+        System.out.println(centroDeportivo.registrarEntrenador(entrenador1)); // true
+        System.out.println(centroDeportivo.registrarEntrenador(entrenador2));
+        System.out.println(centroDeportivo.registrarEntrenador(entrenador3));// true
+
+        System.out.println(centroDeportivo.mostrarEntrenadores());
+
+
+
 
     }
 }
